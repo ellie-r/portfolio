525bedada5afcc355212a8c58e2d75127b8b8f36
Ellie <erobinson98@btinternet.com>
2020-12-01 20:34:44 +0000

    added border to browser images if in light mode

--- app/js/app.ts
+++ app/js/app.ts
@@ -2,16 +2,24 @@ const root: HTMLElement = document.documentElement;
 const imageOfMe: SVGElement = document.querySelector('svg #XMLID_3_');
 const toggle: Element = document.querySelector('.toggle');
 let navCollapse: NodeListOf<Element> = document.querySelectorAll('.nav-link');
+let browserImages: NodeListOf<Element> = document.querySelectorAll('.browser img');
+
 
 toggle.addEventListener('change', (e) => {
     if((e.target as HTMLInputElement).checked) {
         root.style.setProperty('--background', '#FFFFFF')
         root.style.setProperty('--text', '#767D7A')
+        browserImages.forEach( (browser) => {
+            (browser as HTMLElement).style.border = '1px solid #e8eaed';
+        })
         imageOfMe.style.fill = 'var(--background)';
     }  else {
         root.style.setProperty('--background', '#767D7A')
         root.style.setProperty('--text', '#FFFFFF')
         imageOfMe.style.fill = 'var(--background)';
+        browserImages.forEach( (browser) => {
+            (browser as HTMLElement).style.border = '';
+        })
     }
 })
 
--- app/js/dist/app.js
+++ app/js/dist/app.js
@@ -2,16 +2,23 @@ var root = document.documentElement;
 var imageOfMe = document.querySelector('svg #XMLID_3_');
 var toggle = document.querySelector('.toggle');
 var navCollapse = document.querySelectorAll('.nav-link');
+var browserImages = document.querySelectorAll('.browser img');
 toggle.addEventListener('change', function (e) {
     if (e.target.checked) {
         root.style.setProperty('--background', '#FFFFFF');
         root.style.setProperty('--text', '#767D7A');
+        browserImages.forEach(function (browser) {
+            browser.style.border = '1px solid #e8eaed';
+        });
         imageOfMe.style.fill = 'var(--background)';
     }
     else {
         root.style.setProperty('--background', '#767D7A');
         root.style.setProperty('--text', '#FFFFFF');
         imageOfMe.style.fill = 'var(--background)';
+        browserImages.forEach(function (browser) {
+            browser.style.border = '';
+        });
     }
 });
 navCollapse.forEach(function (link) {
